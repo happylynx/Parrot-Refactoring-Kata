7c4712f7a9a345200af21e566354c0bbcb4cdd07
happylynx <happylynx@localhost>
2023-02-21 03:19:48 +0100

    http(s) proxy from env variables

--- Java/build.gradle.kts
+++ Java/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.net.URI
+
 // Apply the java plugin to add support for Java
 plugins {
     id("java")
@@ -26,4 +28,39 @@ tasks.getByName<Test>("test") {
 tasks.wrapper {
     gradleVersion = "8.0.1"
 }
+
+setProxy()
+
+fun setProxy() {
+    System.getenv("http_proxy").apply {
+        if (isNullOrBlank()) {
+            return
+        }
+        val uri = URI.create(this)
+        System.setProperty("http.proxyHost", uri.host)
+        System.setProperty("http.proxyPort", uri.port.toString())
+    }
+    System.getenv("https_proxy").apply {
+        if (isNullOrBlank()) {
+            return
+        }
+        val uri = URI.create(this)
+        System.setProperty("https.proxyHost", uri.host)
+        System.setProperty("https.proxyPort", uri.port.toString())
+    }
+    System.getenv("no_proxy").apply {
+        if (isNullOrBlank()) {
+            return
+        }
+        val nonProxyHosts = this.splitToSequence(",")
+            .map {
+                if (!it.startsWith('.')) {
+                    return@map it
+                }
+                return@map "*$it"
+            }.joinToString("|")
+        System.setProperty("http.nonProxyHosts", nonProxyHosts)
+        System.setProperty("https.nonProxyHosts", nonProxyHosts)
+    }
+    println("proxy set")
 }
